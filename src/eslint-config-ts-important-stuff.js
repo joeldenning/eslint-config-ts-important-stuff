c57ce9430969a8c2ef8b0632477ac30086ca33a3
Joel Denning <joeldenning@gmail.com>
2019-08-08 18:41:49 -0600

    Initial implementation

---
+++ src/eslint-config-ts-important-stuff.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "important-stuff",
+  ],
+  "rules": {
+    // https://github.com/babel/babel-eslint/issues/760
+    // https://twitter.com/kentcdodds/status/1144963049469747201
+    "no-undef": "off",
+  },
+}
